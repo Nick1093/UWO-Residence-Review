c7ad783f3d50061d2a6a27df8902508676ff093f
Nicholas Lam <nicholasalexanderlam@gmail.com>
2023-01-30 09:56:26 -0500

    starting new day, pushing changes from yesterday primarily fixing some errors and mapping out objects of universities instead of multiple graph parts

---
+++ src/components/HomePage.js
@@ -0,0 +1,59 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  const residences = [
+    {
+      name: "Ontario Hall",
+      image: "../images/Ontario-Hall.png",
+    },
+    {
+      name: "Perth",
+      image: "../images/Perth.png",
+    },
+    {
+      name: "Medsyd",
+      image: "../images/Medsyd.png",
+    },
+    {
+      name: "Essex",
+      image: "../images/Essex.png",
+    },
+    {
+      name: "Saugeen",
+      image: "../images/Saugeen.png",
+    },
+    {
+      name: "London Hall",
+      image: "../images/London-Hall.png",
+    },
+    {
+      name: "Delaware",
+      image: "../images/Delaware.png",
+    },
+  ];
+
+  return (
+    <>
+      <h1 className="title">Residences</h1>
+      <Grid container>
+        {residences.map((building) => (
+          <>
+            <Grid item xs={4} className="Grid-item">
+              <Paper elevation={24}>
+                <img src={building.image} alt={building.name}></img>
+                <Link to={`/reviews-page/${building.name}`}>
+                  {building.name}
+                </Link>
+              </Paper>
+            </Grid>
+          </>
+        ))}
+      </Grid>
+    </>
+  );
+}
+
+export default HomePage;
